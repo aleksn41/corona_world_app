d9ae6fb7e22ccc74c8039f4a2be414b4112f8798
Thomas <thomasmeier2077@t-online.de>
2020-11-14 17:12:38 +0100

    updated structure

--- app/src/main/java/de/dhbw/corona_world_app/StatisticServices.java
+++ app/src/main/java/de/dhbw/corona_world_app/StatisticServices.java
@@ -1,4 +1,10 @@
 package de.dhbw.corona_world_app;
 
+import java.util.List;
+
 public class StatisticServices {
+
+    public static List<Country> getStatistic(List<Country> countryList, Charttype charttype, List<Criteria> criteriaList){
+        return null;
+    }
 }
