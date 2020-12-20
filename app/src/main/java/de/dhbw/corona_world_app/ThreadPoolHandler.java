012152a5086502e663b9c5df9caad392b64e7466
Thomas <thomasmeier2077@t-online.de>
2020-12-20 14:52:03 +0100

    +added ThreadPoolHandler

---
+++ app/src/main/java/de/dhbw/corona_world_app/ThreadPoolHandler.java
@@ -0,0 +1,25 @@
+package de.dhbw.corona_world_app;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolHandler {
+    static final int DEFAULT_THREAD_POOL_SIZE = Runtime.getRuntime().availableProcessors()*2 + 1;
+
+    private static ExecutorService sInstance;
+
+    public static ExecutorService executorService;
+
+    public ExecutorService getExecutorService() {
+        return executorService;
+    }
+
+    public static ExecutorService getsInstance() {
+        if (sInstance == null) {
+            synchronized (ExecutorService.class) {
+                sInstance = Executors.newFixedThreadPool(DEFAULT_THREAD_POOL_SIZE);
+            }
+        }
+        return sInstance;
+    }
+}
