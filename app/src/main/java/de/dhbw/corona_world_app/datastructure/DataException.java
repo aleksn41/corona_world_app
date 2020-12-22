7f1000012b04992d910cc90181b2db320dc23eed
Aleksn41 <Aleksn41@gmail.com>
2020-12-22 20:52:45 +0100

    updated errorMessage

--- app/src/main/java/de/dhbw/corona_world_app/datastructure/DataException.java
+++ app/src/main/java/de/dhbw/corona_world_app/datastructure/DataException.java
@@ -3,7 +3,7 @@ package de.dhbw.corona_world_app.datastructure;
 public class DataException extends Error{
 
     public DataException(){
-        super("There seems to be a Problem with your local Data");
+        super("There seems to be a problem with your local data");
     }
 
     public DataException(String message){
