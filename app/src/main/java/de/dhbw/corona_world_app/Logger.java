d0be8e755df9c3997ee2ff88a3b2b3d35ef96ae9
Thomas Meier <t.meier@ts.daun.tgr.local>
2020-12-28 14:57:49 +0100

    +improved Logger to support verbose and throwable in parameter of logE

--- app/src/main/java/de/dhbw/corona_world_app/Logger.java
+++ app/src/main/java/de/dhbw/corona_world_app/Logger.java
@@ -18,6 +18,18 @@ public class Logger {
         }
     }
 
+    public static void logE(String tag, String msg, Throwable throwable){
+        if(logOn==true){
+            Log.e(tag, msg, throwable);
+        }
+    }
+
+    public static void logV(String tag, String msg){
+        if(logOn==true){
+            Log.v(tag, msg);
+        }
+    }
+
     public static void disableLogging(){
         logOn = false;
     }
