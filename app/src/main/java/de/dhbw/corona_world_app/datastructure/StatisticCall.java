e27cf913c9028e94641558c74b42907e234d79ce
Thomas <thomasmeier2077@t-online.de>
2021-01-05 20:29:58 +0100

    +improved StatisticCall, added time frame

--- app/src/main/java/de/dhbw/corona_world_app/datastructure/StatisticCall.java
+++ app/src/main/java/de/dhbw/corona_world_app/datastructure/StatisticCall.java
@@ -1,18 +1,31 @@
 package de.dhbw.corona_world_app.datastructure;
 
+import androidx.annotation.NonNull;
+
+import java.time.LocalDate;
 import java.util.List;
 
 public class StatisticCall {
+    public static final LocalDate MIN_DATE = LocalDate.of(2020,1,22);
+
     private List<ISOCountry> countryList;
 
     private Charttype charttype;
 
     private List<Criteria> criteriaList;
 
-    public StatisticCall(List<ISOCountry> countryList, Charttype charttype, List<Criteria> criteriaList) {
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    public StatisticCall(List<ISOCountry> countryList, Charttype charttype, List<Criteria> criteriaList, @NonNull LocalDate startDate, LocalDate endDate) {
+        if(startDate.isBefore(MIN_DATE)) throw new IllegalArgumentException("Parameter \"startDate\"=" + startDate.toString() + " is too early! Expected Date is after 21.01.2020.");
+        if(endDate.isBefore(startDate)) throw new IllegalArgumentException("Parameter \"endDate\"=" + endDate.toString() + " is before parameter \"startDate\"!");
         this.countryList = countryList;
         this.charttype = charttype;
         this.criteriaList = criteriaList;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
 
     public List<ISOCountry> getCountryList() {
@@ -37,5 +50,23 @@ public class StatisticCall {
 
     public void setCriteriaList(List<Criteria> criteriaList) {
         this.criteriaList = criteriaList;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(@NonNull LocalDate startDate) {
+        if(startDate.isBefore(MIN_DATE)) throw new IllegalArgumentException("Parameter \"startDate\"=" + startDate.toString() + " is too early! Expected Date is after 21.01.2020.");
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        if(endDate.isBefore(startDate)) throw new IllegalArgumentException("Parameter \"endDate\"=" + endDate.toString() + " is before parameter \"startDate\"!");
+        this.endDate = endDate;
     }
 }
