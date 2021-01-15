f6b4bf23ca08b1e1f69b8b3f9bc13debb640fcd9
Thomas Meier <t.meier@ts.daun.tgr.local>
2021-01-15 15:11:12 +0100

    +added ChartProvider to provide the charts which the statistic services need

---
+++ app/src/main/java/de/dhbw/corona_world_app/statistic/ChartProvider.java
@@ -0,0 +1,16 @@
+package de.dhbw.corona_world_app.statistic;
+
+public class ChartProvider {
+
+    public Object getBarChart(){
+        return null;
+    }
+
+    public Object getPieChart(){
+        return null;
+    }
+
+    public Object getLineChart(){
+        return null;
+    }
+}
--- app/src/main/java/de/dhbw/corona_world_app/statistic/StatisticServices.java
+++ app/src/main/java/de/dhbw/corona_world_app/statistic/StatisticServices.java
@@ -1,15 +1,26 @@
 package de.dhbw.corona_world_app.statistic;
 
+import java.io.ObjectStreamException;
 import java.util.List;
 
 import de.dhbw.corona_world_app.datastructure.ChartType;
 import de.dhbw.corona_world_app.datastructure.Country;
 import de.dhbw.corona_world_app.datastructure.Criteria;
+import de.dhbw.corona_world_app.datastructure.StatisticCall;
 
 public class StatisticServices {
 
-    public static List<Country> getStatistic(List<String> countryList, ChartType charttype, List<Criteria> criteriaList){
-        return null;
-    }
+    private ChartProvider chartProvider;
 
+    public Object getStatistic(StatisticCall statisticCall){
+        if(this.chartProvider==null){
+            chartProvider = new ChartProvider();
+        }
+        switch(statisticCall.getChartType()){
+            case BAR: return chartProvider.getBarChart();
+            case PIE: return chartProvider.getPieChart();
+            case LINE: return chartProvider.getLineChart();
+            default: throw new IllegalArgumentException("This chart type is not accepted!");
+        }
+    }
 }
